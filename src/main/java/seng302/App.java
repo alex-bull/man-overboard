f65280038fc65fb73463060ecb13e6da48fe131f
Pang <psu43@uclive.ac.nz>
2017-03-14 10:06:08 +1300

    FXML loaded
    # story [488] #chore

--- src/main/java/seng302/App.java
+++ src/main/java/seng302/App.java
@@ -1,12 +1,33 @@
 package seng302;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class App
+public class App extends Application
 {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+
     public static void main( String[] args )
     {
+        launch(args);
+
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number of boats in Regatta: ");
         int numberOfBoats;
