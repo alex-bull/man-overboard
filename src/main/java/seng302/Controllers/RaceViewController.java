5ece126ed1f66b3d80438b269ac9fb99e17de708
Mike Lee <msl47@uclive.ac.nz>
2017-03-16 11:52:10 +1300

    Connect the two points with a line - A line is shown between two points for a gate on a canvas
    #implement #story[491] #test #pair[psu43,msl47]

--- src/main/java/seng302/Controllers/RaceViewController.java
+++ src/main/java/seng302/Controllers/RaceViewController.java
@@ -8,6 +8,8 @@ import seng302.Model.Competitor;
 import seng302.Model.CourseFeature;
 import seng302.Model.Regatta;
 
+import java.util.DoubleSummaryStatistics;
+
 /**
  * Controller for the race view.
  */
@@ -25,12 +27,22 @@ public class RaceViewController {
     }
 
     private void draw(GraphicsContext gc) {
+
         for(CourseFeature b:regatta.getPoints()){
             gc.setFill(Color.GREEN);
-            System.out.printf(b.getName());
+            gc.setStroke(Color.BLUE);
+
             if (b.isGate()) {
-                gc.fillOval(b.getLocations().get(0).getX(), b.getLocations().get(0).getY(), 10, 10);
-                gc.fillOval(b.getLocations().get(1).getX(), b.getLocations().get(1).getY(), 10, 10);
+                gc.setLineWidth(3);
+                int w = 15;
+                int h = 15;
+                Double x1 = b.getLocations().get(0).getX();
+                Double y1 =  b.getLocations().get(0).getY();
+                Double x2 = b.getLocations().get(1).getX();
+                Double y2 = b.getLocations().get(1).getY();
+                gc.strokeLine(x1 , y1 , x2+w, y2+h);
+                gc.fillOval(x1, y1, w, h);
+                gc.fillOval(x2, y2, w, h);
             }
             else {
                 gc.fillOval(b.getLocations().get(0).getX(), b.getLocations().get(0).getY(), 20, 20);
