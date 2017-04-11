5e843897651608d123a90a9b6656f258bfede273
Pang <psu43@uclive.ac.nz>
2017-04-11 15:23:22 +1200

    Refactor the data receiver main method. - Moved code for parsing data into Packet class - Documentation, code is much cleaner now
    #story[821]

--- src/main/java/seng302/Model/DataReceiver.java
+++ src/main/java/seng302/Model/DataReceiver.java
@@ -3,32 +3,22 @@ package seng302.Model;
 import seng302.Parsers.Packet;
 
 import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Set;
 
 /**
  * Created by khe60 on 10/04/17.
  * The Receiver class, currently receives messages 1 byte at a time
- * Can't connect to the test port for some reason
+ * Can't connect to the test port for some reason (internet enabler)
  */
 public class DataReceiver {
     private Socket receiveSock;
     private DataInputStream dis;
 
     /**
-     * initializes port to receive binary data from
-     * @param host host of the server
-     * @param port port of the server
-     * @throws IOException
+     * Initializes port to receive binary data from
+     * @param host String host of the server
+     * @param port int number of port of the server
+     * @throws IOException IOException
      */
     public DataReceiver(String host, int port) throws IOException {
         receiveSock = new Socket(host, port);
@@ -37,8 +27,8 @@ public class DataReceiver {
 
 
     /**
-     * close the established streams and sockets
-     * @throws IOException
+     * Close the established streams and sockets
+     * @throws IOException IOException
      */
     public void close() throws IOException {
         receiveSock.close();
@@ -46,24 +36,23 @@ public class DataReceiver {
     }
 
     /**
-     * receives one byte from server and returns it, test server only sends one byte at a time so this is gonna get changed
+     * Receives one byte from server and returns it, test server only sends one byte at a time so this is gonna get changed
      * @return the byte received
      * @throws IOException
      */
     public byte[] receive() throws IOException {
-
-
-
         byte[] received=new byte[1024];
-
         dis.readFully(received);
         return received;
 
     }
+
+
     public static void main (String [] args) throws InterruptedException {
         Packet packet = new Packet();
-        DataReceiver me= null;
-        System.out.println("start connection to server");
+        DataReceiver me = null;
+        System.out.println("Start connection to server...");
+
         while(me==null){
             try {
                 me=new DataReceiver("livedata.americascup.com",4941);
@@ -75,9 +64,8 @@ public class DataReceiver {
 
         }
 
-
         while(true){
-            byte[] msg= new byte[1024];
+            byte[] msg = new byte[1024];
             try {
                 msg = me.receive();
 
@@ -85,57 +73,7 @@ public class DataReceiver {
                 e.printStackTrace();
             }
 
-            int count = 0;
-            boolean isHeader = false;
-            boolean getBoatLocationMsg = false;
-            ArrayList arrayList = new ArrayList();
-            ArrayList msgBody = new ArrayList();
-            int bodyCount = 0;
-            String syncByte1 = "";
-            for(int i=0;i<1024;i++){
-
-                String hex = String.format("%02X", msg[i]);
-                if(hex.equals("83") && syncByte1.equals("47")) {
-                    arrayList.add("47");
-                    count++;
-                    isHeader = true;
-                }
-
-                if(isHeader) {
-                    if(count < 15) {
-                        arrayList.add(hex);
-                        count++;
-                    }
-                    else{
-                        boolean valid  = packet.validBoatLocation(arrayList);
-                        if(valid) {
-                            getBoatLocationMsg = true;
-                        }
-                        count = 0;
-                        arrayList.clear();
-                        isHeader = false;
-
-                    }
-                }
-
-                if(getBoatLocationMsg && bodyCount < 57) {
-//                    System.out.println(hex);
-                    msgBody.add(hex);
-                    bodyCount++;
-                }
-                if (bodyCount == 56) {
-                    System.out.println(msgBody);
-                    packet.processMsgBody(msgBody);
-                    getBoatLocationMsg = false;
-                    msgBody.clear();
-                    bodyCount = 0;
-                }
-
-                syncByte1 = hex;
-
-//                System.out.print(String.format("%02X ", msg[i]));
-
-            }
+            packet.receiveData(msg);
 
         }
     }
--- src/main/java/seng302/Parsers/Packet.java
+++ src/main/java/seng302/Parsers/Packet.java
@@ -1,25 +1,99 @@
 package seng302.Parsers;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by psu43 on 10/04/17.
+ * Handles parsing for received data packets
  */
 public class Packet {
 
 
+    /**
+     * Receive data packets
+     * @param msg byte[] byte array of the message that is being received
+     */
+    public void receiveData(byte[] msg) {
+        int count = 0;
+        int bodyCount = 0;
+        boolean isHeader = false;
+        boolean getBoatLocationMsg = false;
 
-    public boolean validBoatLocation(ArrayList<String> data) {
-        if(data.get(2).equals("25") && data.get(13).equals("38")) {
-//            System.out.println(data);
+        List temp = new ArrayList(); // temporary used list to store bytes
+        List msgBody = new ArrayList();
+
+        String syncByte1 = "";
+
+        // specify expected sync byte values
+        String syncByte1Value = "47";
+        String syncByte2Value = "83";
+        int msgSize = 1024;
+        int headerSize = 15;
+
+        for(int i=0 ; i < msgSize; i++) {
+
+            String hexByte = String.format("%02X", msg[i]);
+
+            // if current byte is equal to sync byte 2 and prev byte is sync byte 1 then flag is header
+            if (hexByte.equals(syncByte2Value) && syncByte1.equals(syncByte1Value)) {
+                temp.add(syncByte1Value);
+                count++;
+                isHeader = true;
+            }
+
+            if (isHeader) {
+                if (count < headerSize) {
+                    temp.add(hexByte);
+                    count++;
+                } else {
+                    if (validBoatLocation(temp)) {
+                        getBoatLocationMsg = true; // flag to parse boat location message
+                    }
+                    // get ready to parse new message
+                    count = 0;
+                    temp.clear();
+                    isHeader = false;
+                }
+            }
+
+            if (getBoatLocationMsg && bodyCount < 57) {
+//                    System.out.println(hex);
+                msgBody.add(hexByte);
+                bodyCount++;
+            }
+            if (bodyCount == 56) {
+                System.out.println(msgBody);
+                processMsgBody(msgBody);
+                getBoatLocationMsg = false;
+                msgBody.clear();
+                bodyCount = 0;
+            }
+
+            syncByte1 = hexByte;
+        }
+    }
+
+    /**
+     * Confirm the message's message type and message length with protocol
+     * @param data List a list of data in hexadecimal bytes
+     * @return boolean True if the data received is a valid boat location
+     */
+    private boolean validBoatLocation(List data) {
+        // from protocol byte 2: message type = 37 in dec and 13: length is 57 in dec
+        String hexMsgType = "25";
+        String hexMsgLen = "38";
+        if(data.get(2).equals(hexMsgType) && data.get(13).equals(hexMsgLen)) {
             return true;
         }
         return false;
     }
 
-    public void processMsgBody(ArrayList<String> body) {
+
+    /**
+     * Process the given list of data and parse it
+     * @param body List a list of hexadecimal bytes
+     */
+    private void processMsgBody(List<String> body) {
         // get source id, latitude, long, heading, speed
         List sourceID = body.subList(7, 11);
         System.out.println("sourceID " + sourceID);
