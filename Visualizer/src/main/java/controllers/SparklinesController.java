5ad5b3175ef92bb51e686ddb7d309eddcf536214
Pang Suwanaposee <psu43@uclive.ac.nz>
2017-05-16 19:28:03 +1200

    Names for each competitor is shown as a name for a series instead on the x axis. - Figured out that name for a series do not show if it doesn't fit within the width of the graph.
    #story[692]

--- Visualizer/src/main/java/controllers/SparklinesController.java
+++ Visualizer/src/main/java/controllers/SparklinesController.java
@@ -5,11 +5,14 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import models.Competitor;
 import utilities.DataSource;
+import utilities.Interpreter;
 
 import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by abu59
@@ -21,8 +24,9 @@ public class SparklinesController {
     @FXML private LineChart<String, Double> sparkChart;
 
     private List<Competitor> competitors;
-    private ArrayList<XYChart.Series> seriesList;
+    private ArrayList<XYChart.Series<String, Double>> seriesList;
     private DataSource dataSource;
+    private Map<String, XYChart.Series> seriesMap = new HashMap<>();
 
 
     void setCompetitors(DataSource dataSource){
@@ -32,41 +36,59 @@ public class SparklinesController {
         seriesList = new ArrayList<>();
         System.out.println("init: " + seriesList);
 
-        XYChart.Series<String, Double> series = new XYChart.Series<>();
+
 
         //series.getData().add(new XYChart.Data<>("A", 15.1));
         System.out.println(competitors.size());
         //series.getData().add(new XYChart.Data(2,2));
+//        XYChart.Series<String, Double> series = new XYChart.Series<>();
+//        series.setName("Test");
+//        series.getData().add(new XYChart.Data<>("Start",0.0));
+//        for(Competitor boat: competitors) {
+//            series.getData().add(new XYChart.Data<>("Start",0.0));
+//
+//        }
 
+//        sparkChart.getData().add(series);
 
-        for(Competitor boat: competitors) {
-            series.getData().add(new XYChart.Data<>(boat.getAbbreName(),2.2));
-
-        }
-
-        sparkChart.getData().add(series);
-        /*
         for(Competitor boat: competitors){
+
             System.out.println("setComp: " + seriesList);
+            XYChart.Series<String, Double> series = new XYChart.Series<>();
             seriesList.add(series);
             System.out.println(competitors.indexOf(boat));
-            //seriesList.get(competitors.indexOf(boat)).getNode().setStyle("-fx-stroke: " + boat.getColor());
-            seriesList.get(competitors.indexOf(boat)).getData().add(new XYChart.Data(seriesList.size() + 1, 3)); //replace y with team position
-            sparkChart.getData().add(seriesList.get(competitors.indexOf(boat)));
+
+            XYChart.Series<String, Double> currentSeries = seriesList.get(competitors.indexOf(boat));
+//            currentSeries.getNode().setStyle("-fx-stroke: blueviolet");
+            currentSeries.getData().add(new XYChart.Data<>(Integer.toString(seriesList.size() + 1), 3.0)); //replace y with team position
+            sparkChart.getData().add(currentSeries);
+            currentSeries.setName(boat.getAbbreName());
+            currentSeries.getNode().setStyle("-fx-stroke: " + boat.getColor());
+//
+
         }
 
-        */
+
 
     }
 
 
-    public void refresh(){
+    void refresh(){
+//        for(Competitor boat: competitors) {
+//            series.getData().add(new XYChart.Data<>(boat.getAbbreName(),2.2));
+//
+//        }
+//        Competitor boat = this.competitors.get(0);
+//        series.getData().add(new XYChart.Data<>(boat.getAbbreName(),2.2));
+//        System.out.println(series.getData().size());
 
+        /*
         for(Competitor boat: competitors){
             //seriesList.get(competitors.indexOf(boat)).getNode().setStyle("-fx-stroke: " + boat.getColor());
             seriesList.get(competitors.indexOf(boat)).getData().add(new XYChart.Data(seriesList.size() + 1, 3)); //replace y with team position
             sparkChart.getData().add(seriesList.get(competitors.indexOf(boat)));
         }
+        */
     }
 
 
