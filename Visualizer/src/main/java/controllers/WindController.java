bb5ed22373b5d22859c1f6e1b8d568cee258aaa3
Pang Suwanaposee <psu43@uclive.ac.nz>
2017-05-19 16:30:12 +1200

    Wind arrow length passed into the windController but the length of arrow is not changing drastically due to small change in wind speed. Still needs developing.
    #story[977] #pair[mgo65, psu43]

--- Visualizer/src/main/java/controllers/WindController.java
+++ Visualizer/src/main/java/controllers/WindController.java
@@ -10,6 +10,8 @@ import utilities.DataSource;
 
 import javax.xml.crypto.Data;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static javafx.scene.paint.Color.BLACK;
@@ -23,6 +25,7 @@ public class WindController implements Initializable{
     @FXML Pane windPane;
     @FXML private Text speed;
     private Polygon arrow;
+    private List<Double> arrowPoints = new ArrayList<>();
 
 
     @Override
@@ -30,9 +33,27 @@ public class WindController implements Initializable{
         this.arrow = new Polygon();
         arrow.setLayoutX(80);
         arrow.setLayoutY(80);
+
+//        arrowPoints.add(-5.0);
+//        arrowPoints.add(10.); //tail left
+//        arrowPoints.add(5.0);
+//        arrowPoints.add(10.0); // tail right
+//        arrowPoints.add(5.0);
+//        arrowPoints.add(-20.0);
+//        arrowPoints.add(15.0);
+//        arrowPoints.add(-20.0);
+//        arrowPoints.add(0.0);
+//        arrowPoints.add(-40.0);
+//        arrowPoints.add(-15.0);
+//        arrowPoints.add(-20.0);
+//        arrowPoints.add(-5.0);
+//        arrowPoints.add(-20.0);
+//
+//        arrow.getPoints().addAll(arrowPoints);
+
         arrow.getPoints().addAll(
-                -5., 10., //tail left
-                5., 10., //tail right
+                -5., -10., //tail left
+                5., -10., //tail right
                 5., -20.,
                 15., -20.,
                 0., -40., // tip
@@ -54,8 +75,25 @@ public class WindController implements Initializable{
         arrow.toFront();
         arrow.getTransforms().clear();
         arrow.getTransforms().add(new Rotate(angle, 0, 0));
+        System.out.println(angle  + "  WIND SPEED " + windSpeed);
+
+//        Double f = (windSpeed -5) * 10;
+        Integer f = (int) windSpeed * 10;
+        System.out.println("FACTOR " + f);
+        //        arrowPoints.set(1, arrowPoints.get(1) * factor);
+//        arrowPoints.set(3, arrowPoints.get(3) * factor);
+
+        arrow.getPoints().addAll(
+        -5., -19. + f, //tail left
+        5., -19. + f, //tail right
+        5., -20.,
+        15., -20.,
+        0., -40., // tip
+        -15., -20.,
+        -5.,-20.);
+
 //        System.out.println(arrow.getPoints());
-//        double factor = windSpeed/6.0;
+
 //        arrow.getPoints().
 //        arrow.getPoints().get(1) = arrow.getPoints().get(1) * factor;
 
