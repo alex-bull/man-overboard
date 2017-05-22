0d6d5982e367adadffa8dc9abf6239542b45890e
Pang Suwanaposee <psu43@uclive.ac.nz>
2017-05-22 17:03:07 +1200

    Scaling wind speed so that the arrow display is reasonable. - Arrow length is limited to a set minimum and maximum - Had to multiply wind by a factor so that changes in the length can be seen clearly - Modified FXML so that the wind arrow should not overlap text - There was a problem  where FPS was dropping because points were not cleared but it is now fixed - There was also a problem with the rotation of the arrow because the pivot was not set to the center of the arrow.
    #story[977] #fix

--- Visualizer/src/main/java/controllers/WindController.java
+++ Visualizer/src/main/java/controllers/WindController.java
@@ -6,12 +6,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
-import utilities.DataSource;
-
-import javax.xml.crypto.Data;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import static javafx.scene.paint.Color.BLACK;
@@ -25,7 +20,6 @@ public class WindController implements Initializable{
     @FXML Pane windPane;
     @FXML private Text speed;
     private Polygon arrow;
-    private List<Double> arrowPoints = new ArrayList<>();
 
 
     @Override
@@ -33,26 +27,8 @@ public class WindController implements Initializable{
         this.arrow = new Polygon();
         arrow.setLayoutX(80);
         arrow.setLayoutY(80);
-
-//        arrowPoints.add(-5.0);
-//        arrowPoints.add(10.); //tail left
-//        arrowPoints.add(5.0);
-//        arrowPoints.add(10.0); // tail right
-//        arrowPoints.add(5.0);
-//        arrowPoints.add(-20.0);
-//        arrowPoints.add(15.0);
-//        arrowPoints.add(-20.0);
-//        arrowPoints.add(0.0);
-//        arrowPoints.add(-40.0);
-//        arrowPoints.add(-15.0);
-//        arrowPoints.add(-20.0);
-//        arrowPoints.add(-5.0);
-//        arrowPoints.add(-20.0);
-//
-//        arrow.getPoints().addAll(arrowPoints);
-
         arrow.getPoints().addAll(
-                -5., -10., //tail left
+                -5., -10.0, //tail left
                 5., -10., //tail right
                 5., -20.,
                 15., -20.,
@@ -72,31 +48,25 @@ public class WindController implements Initializable{
      * @param windSpeed double the wind speed in m/s
      */
     void refresh(double angle, double windSpeed) {
+        int minWind = 5;
+        int maxWind = 7;
+        double factor = 40.0;
+        Double windFactor = windSpeed * factor;
+        double offset = minWind * factor;
         arrow.toFront();
         arrow.getTransforms().clear();
-        arrow.getTransforms().add(new Rotate(angle, 0, 0));
-        System.out.println(angle  + "  WIND SPEED " + windSpeed);
-
-//        Double f = (windSpeed -5) * 10;
-        Integer f = (int) windSpeed * 10;
-        System.out.println("FACTOR " + f);
-        //        arrowPoints.set(1, arrowPoints.get(1) * factor);
-//        arrowPoints.set(3, arrowPoints.get(3) * factor);
-
-        arrow.getPoints().addAll(
-        -5., -19. + f, //tail left
-        5., -19. + f, //tail right
-        5., -20.,
-        15., -20.,
-        0., -40., // tip
-        -15., -20.,
-        -5.,-20.);
-
-//        System.out.println(arrow.getPoints());
-
-//        arrow.getPoints().
-//        arrow.getPoints().get(1) = arrow.getPoints().get(1) * factor;
-
+        arrow.getTransforms().add(new Rotate(angle, 0, 25));
+        if(windSpeed >= minWind && windSpeed <= maxWind) {
+            arrow.getPoints().remove(0,14);
+            arrow.getPoints().addAll(
+                    -5., -offset + windFactor, //tail left
+                    5., -offset + windFactor, //tail right
+                    5., -20.,
+                    15., -20.,
+                    0., -40., // tip
+                    -15., -20.,
+                    -5.,-20.);
+        }
         speed.setText(Double.toString(windSpeed));
     }
 
