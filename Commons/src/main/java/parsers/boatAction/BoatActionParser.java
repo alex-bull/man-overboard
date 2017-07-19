0f756085a0e500c467543b7f25c4f0c8f8fbd830
Mike Lee <msl47@uclive.ac.nz>
2017-07-19 13:56:37 +1200

    Created a new parser for boat action messages. Also created a class that stores boat action data.
    #story[1048]

---
+++ Commons/src/main/java/parsers/boatAction/BoatActionParser.java
@@ -0,0 +1,31 @@
+package parsers.boatAction;
+
+import java.util.Arrays;
+
+import static parsers.Converter.hexByteArrayToInt;
+
+/**
+ * Created by msl47 on 19/07/17.
+ */
+public class BoatActionParser {
+
+//TODO: figure out binary message format
+
+//    public boatActionData processMessage (byte[] body) {
+//        Message Type: 100 (0x064)
+//        Message Size: 1 byte (always 1)
+//        Boat Action (1 byte):
+//        1 Autopilot/VMG
+//        2 Sails in
+//        3 Sails out
+//        4 Tack/Gybe
+//        5 Upwind
+//        6 Downwind
+
+//        try{
+//            Integer size = hexByteArrayToInt(Arrays.copyOfRange(body, , 11));
+//
+//        }
+//    }
+
+}
---
+++ Commons/src/main/java/parsers/boatAction/boatActionData.java
@@ -0,0 +1,7 @@
+package parsers.boatAction;
+
+/**
+ * Created by msl47 on 19/07/17.
+ */
+public class boatActionData {
+}
