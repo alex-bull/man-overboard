864cd906a6f28180aec9d8d41424d60c21a88b7a
Isabella Joe <ikj11@uclive.ac.nz>
2017-08-15 19:10:34 +1200

    Fixed HealthColourCalculationTest
    #Story[1125]

--- Visualizer/src/test/java/utilities/HealthColourCalculationTest.java
+++ Visualizer/src/test/java/utilities/HealthColourCalculationTest.java
@@ -27,14 +27,14 @@ public class HealthColourCalculationTest {
 
     @Test
     public void calculateHealthColourGreenTest() {
-        boat.updateHealth(1);
+        boat.updateHealth(-1);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.GREEN, resultColour);
     }
 
     @Test
     public void calculateHealthColourGreenTest2() {
-        boat.updateHealth(29);
+        boat.updateHealth(-29);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.GREEN, resultColour);
     }
@@ -42,14 +42,14 @@ public class HealthColourCalculationTest {
 
     @Test
     public void calculateHealthColourGreenYellowTest() {
-        boat.updateHealth(30);
+        boat.updateHealth(-30);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.GREENYELLOW, resultColour);
     }
 
     @Test
     public void calculateHealthColourGreenYellowTest2() {
-        boat.updateHealth(39);
+        boat.updateHealth(-39);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.GREENYELLOW, resultColour);
     }
@@ -57,14 +57,14 @@ public class HealthColourCalculationTest {
 
     @Test
     public void calculateHealthColourYellowTest() {
-        boat.updateHealth(40);
+        boat.updateHealth(-40);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.YELLOW, resultColour);
     }
 
     @Test
     public void calculateHealthColourYellowTest2() {
-        boat.updateHealth(49);
+        boat.updateHealth(-49);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.YELLOW, resultColour);
     }
@@ -73,14 +73,14 @@ public class HealthColourCalculationTest {
 
     @Test
     public void calculateHealthColourOrangeTest() {
-        boat.updateHealth(50);
+        boat.updateHealth(-50);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.ORANGE, resultColour);
     }
 
     @Test
     public void calculateHealthColourOrangeTest2() {
-        boat.updateHealth(59);
+        boat.updateHealth(-59);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.ORANGE, resultColour);
     }
@@ -89,14 +89,14 @@ public class HealthColourCalculationTest {
 
     @Test
     public void calculateHealthColourRedTest() {
-        boat.updateHealth(60);
+        boat.updateHealth(-60);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.RED, resultColour);
     }
 
     @Test
     public void calculateHealthColourRedTest2() {
-        boat.updateHealth(100);
+        boat.updateHealth(-100);
         Color resultColour = calculateHealthColour(boat);
         assertEquals(Color.RED, resultColour);
     }
